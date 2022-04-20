1367f4ee3f601e00ee8757ed9400f484aaef7ab8
yasin0205 <yasinislamhimaloyodesk@gmail.com.com>
2022-04-20 16:20:01 +0600

    url and canonical url

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -27,9 +28,17 @@ class Post(models.Model):
     objects = models.Manager() # The default manager.
     published = PublishedManager() # Our custom manager.
 
+    # Canonical URL for models
+    def get_absolute_url(self):
+        return reverse('blog:post_details',
+                       args=[
+                           self.publish.year,
+                           self.publish.month,
+                           self.publish.day,
+                           self.slug
+                       ])
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
-
